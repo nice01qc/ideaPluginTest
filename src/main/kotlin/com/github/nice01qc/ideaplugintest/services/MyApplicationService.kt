130a511b37f379f1ec014aa27aa2416faf1ef481
GitHub Action <action@github.com>
2020-12-07 14:49:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/nice01qc/ideaplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nice01qc/ideaplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nice01qc.ideaplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nice01qc.ideaplugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nice01qc/ideaplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nice01qc/ideaplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nice01qc.ideaplugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nice01qc.ideaplugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
